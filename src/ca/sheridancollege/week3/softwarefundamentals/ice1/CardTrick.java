74ba7661fcf1f310f7af5af60558acdf2ab97efb
manteshsandhu <singmant@sheridancollege.ca>
2019-05-22 12:07:10 -0400

    Mantesh Sandhu Inclass 1

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ * @author ManteshSandhu
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 import java.util.Scanner;
