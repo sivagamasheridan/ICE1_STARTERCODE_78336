5bc029ba7f6e40689b29b5ac59ced01257495660
mosquejo <50668863+mosquejo@users.noreply.github.com>
2019-05-22 12:41:04 -0400

    Added Author

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+// Author: Jorge A. MOsquera 
+
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
 
 /**
