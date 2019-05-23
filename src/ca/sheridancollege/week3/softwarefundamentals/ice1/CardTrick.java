2f06a1ab40c69f56f1544a941cfa97942f24d276
Ridhima Nanda <Ridhima Nanda@LAPTOP-UIKHFSB3>
2019-05-23 11:40:56 -0700

    StudentDemo

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -9,7 +9,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author Sivagami
+ * @author ksfjlks
  */
 public class CardTrick {
     
