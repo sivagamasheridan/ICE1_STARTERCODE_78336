4f8208344076a0becc6a631668a95e6e2c3f85d0
ridhimananda <50668910+ridhimananda@users.noreply.github.com>
2019-05-26 12:26:28 -0700

    Update Card.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/Card.java
@@ -12,7 +12,7 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author Arsh Singla
+ * @author Ridhima_Nanda
  */
 public class Card {
 
